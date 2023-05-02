226c448bc023c228ac5cde60f0e3f3b0f438f29a
SaaFomenko <s.fomenko@list.ru>
2023-05-02 22:51:51 +0300

    Save

--- main.cpp
+++ main.cpp
@@ -4,5 +4,20 @@
 
 int main()
 {
+	try
+	{
+		pqxx::connection c(
+			"host=localhost "
+			"port=5432 "
+			"dbname=nlogy "
+			"user=nlogy "
+			"password=test"
+		);
+	} 
+	catch (pqxx::sql_error e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 	return 0;
 }
