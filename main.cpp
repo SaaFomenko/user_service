657996172ee786bf3a8d1b9eb31266dfeb54f973
SaaFomenko <s.fomenko@list.ru>
2023-05-03 15:18:18 +0300

    Need create config library.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include <fstream>
 #include <pqxx/pqxx>
+#include "lib/config/config.h"
 
 
 int main()
 {
+	const std::string connect_str = get_connect;
+
 	try
 	{
 		pqxx::connection c(
-			"host=localhost "
-			"port=5432 "
-			"dbname=nlogy "
-			"user=nlogy "
-			"password=test"
 		);
 	} 
 	catch (pqxx::sql_error e)
